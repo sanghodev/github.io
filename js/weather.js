aa0c4c81dc27a3dfe6035e03baed5c8cfccfabc3
sanghodev <sanghodev@gmail.com>
2022-09-11 16:44:17 -0400

    v1

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+ const API_KEY = "e42c1c0dec989bc9ae944fff8f743827";
+
+function onGeoOk(position) {
+  
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  // console.log("You live in", lat, lon);
+  
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
