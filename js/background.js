15a44db99ea4d5767b2d3f2a1d7d5121afb37767
sanghodev <sanghodev@gmail.com>
2022-09-12 00:18:35 -0400

    v1.2

--- js/background.js
+++ js/background.js
@@ -1,11 +1,9 @@
 const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
-bgImage.src = `./img/${chosenImage}`;
-bgImage.className = "set"
-document.body.appendChild(bgImage);
+// const bgImage = document.createElement("img");
+// bgImage.src = `./img/${chosenImage}`;
+// bgImage.className = "set"
+// document.body.appendChild(bgImage);
 
-const body = document.getElementsByTagName(body);
-
-body.style.bgImage = `./img/${chosenImage}`;
+document.body.style.backgroundImage = `url('./img/${chosenImage}')`;
